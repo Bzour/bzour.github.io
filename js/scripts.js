a1d54797f2a6959a31b76e5a1cc4cf278789b00c
Jleloup <jleloup.45@gmail.com>
2019-06-10 19:38:14 +0200

    Changements script

--- js/scripts.js
+++ js/scripts.js
@@ -175,11 +175,13 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
+        if (false && MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+            console.log("Dans le else");
+            // $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbyOt9s4t5lpQqA7y3HSrSpzq5a8qjjmvJgjpu_MsjH2stbiUjw/exec', data)
                 .done(function (data) {
                     console.log(data);
                     $('#alert-wrapper').html('');
